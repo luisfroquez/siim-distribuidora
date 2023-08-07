55534f011106cbb7121299224872d2ded40d20d8
Luis <luisfroquez@gmail.com>
2023-08-06 23:02:56 -0400

    fix build errors

--- src/app/api/update-catalog-per-day/route.ts
+++ src/app/api/update-catalog-per-day/route.ts
@@ -1,36 +1,36 @@
-// import type { NextRequest } from 'next/server'
-// import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 
-// import { updateIwsProducts } from '@/app/_actions/update-products-iws'
+import { updateIwsProducts } from '@/app/_actions/update-products-iws'
 
-// // UPDATES CATALOG EVERY 24HS.
-// export function GET(request: NextRequest) {
-//   return updateIwsProducts()
-//     .then(() => {
-//       return NextResponse.json(
-//         {
-//           body: request.body,
-//           path: request.nextUrl.pathname,
-//           query: request.nextUrl.search,
-//           cookies: request.cookies.getAll(),
-//         },
-//         {
-//           status: 200,
-//         }
-//       )
-//     })
-//     .catch((e: unknown) => {
-//       return NextResponse.json(
-//         {
-//           body: request.body,
-//           path: request.nextUrl.pathname,
-//           query: request.nextUrl.search,
-//           cookies: request.cookies.getAll(),
-//           error: e,
-//         },
-//         {
-//           status: 400,
-//         }
-//       )
-//     })
-// }
+// UPDATES CATALOG EVERY 24HS.
+export function GET(request: NextRequest) {
+  return updateIwsProducts()
+    .then(() => {
+      return NextResponse.json(
+        {
+          body: request.body,
+          path: request.nextUrl.pathname,
+          query: request.nextUrl.search,
+          cookies: request.cookies.getAll(),
+        },
+        {
+          status: 200,
+        }
+      )
+    })
+    .catch((e: unknown) => {
+      return NextResponse.json(
+        {
+          body: request.body,
+          path: request.nextUrl.pathname,
+          query: request.nextUrl.search,
+          cookies: request.cookies.getAll(),
+          error: e,
+        },
+        {
+          status: 400,
+        }
+      )
+    })
+}
