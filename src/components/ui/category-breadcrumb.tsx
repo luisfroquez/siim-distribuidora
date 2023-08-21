c102a36e92f41656107f947fa212815d587c7415
Luis Roque <luisfroquez@gmail.com>
2023-08-20 22:19:03 -0400

    fix: promises in category breadcrumb

--- src/components/ui/category-breadcrumb.tsx
+++ src/components/ui/category-breadcrumb.tsx
@@ -1,5 +1,7 @@
-import { getCategoryBySlug } from '@/wp/get-category-by-slug'
-import { type WpCategories } from '@/wp/types'
+import { client } from '@/lib/apollo/apollo'
+import { GET_CATEGORY_BY_SLUG } from '@/wp/queries'
+import { WpProductCategory } from '@/wp/types'
+import { ApolloQueryResult } from '@apollo/client'
 import Link from 'next/link'
 
 interface CategoryBreadcrumbProps {
@@ -13,22 +15,28 @@ const CategoryBreadcrumb = async ({ uri }: CategoryBreadcrumbProps) => {
     .filter((e) => e !== '/')
     .filter(Boolean)
 
-  const promisesArray: Promise<WpCategories>[] = []
+  const promisesArray: Promise<ApolloQueryResult<WpProductCategory>>[] = []
 
   if (items) {
-    items?.forEach((e) => {
-      const promise = getCategoryBySlug(e)
+    items?.forEach((slug) => {
+      const promise = client.query<WpProductCategory>({
+        query: GET_CATEGORY_BY_SLUG,
+        variables: { slug },
+      })
 
-      promisesArray.push(promise as Promise<WpCategories>)
+      promisesArray.push(
+        promise as Promise<ApolloQueryResult<WpProductCategory>>
+      )
     })
   }
 
-  const [...categories]: WpCategories[] = await Promise.all(promisesArray)
+  const [...categories] = await Promise.all(promisesArray)
 
   return (
     <div className="mb-4 flex w-full gap-2  py-2 text-xs">
-      {categories.map((c, i) => {
+      {categories.map((data, i) => {
         const isLast = i === categories.length - 1
+        const c = data.data.productCategory
 
         return (
           <>
