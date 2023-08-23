cabc6c2c00c6a33d97ff099d113c8e8f38c30852
Luis Roque <luisfroquez@gmail.com>
2023-08-23 12:50:54 -0400

    fix: add credentials

--- src/lib/apollo/apollo.ts
+++ src/lib/apollo/apollo.ts
@@ -3,4 +3,5 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 export const client = new ApolloClient({
   uri: 'http://wp.siim.cl/graphql',
   cache: new InMemoryCache(),
+  credentials: 'include',
 })
