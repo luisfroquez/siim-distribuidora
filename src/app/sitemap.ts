1121b93cff30c0b5745fd54d5c9dc88142556746
Luis Roque <luisfroquez@gmail.com>
2023-08-31 15:45:58 -0400

    feat: add new screenshot, new favicon and robots.txt file

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,6 +9,26 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   })
 
   return [
+    {
+      url: 'https://distribuidora.siim.cl',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://distribuidora.siim.cl/blog',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://distribuidora.siim.cl/contacto',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://distribuidora.siim.cl/signin',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://distribuidora.siim.cl/signup',
+      lastModified: new Date(),
+    },
     ...data.products.nodes.map(({ slug }) => ({
       url: `https://distribuidora.siim.cl/tienda/producto/${slug ?? ''}`,
       lastModified: new Date(),
