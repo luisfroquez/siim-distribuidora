c847239064ee53a710157a6af6bec5c312275d61
Luis Roque <luisfroquez@gmail.com>
2023-09-11 10:07:54 -0300

    feat: mdx

--- src/app/(lobby)/ayuda/como-cotizar/page.tsx
+++ src/app/(lobby)/ayuda/como-cotizar/page.tsx
@@ -4,7 +4,7 @@ import HelloWorld from './hello.mdx'
 
 export default function Page() {
   return (
-    <article className="prose lg:prose-xl  dark:prose-invert ">
+    <article className="prose lg:prose-xl  dark:prose-invert max-w-5xl mx-auto py-16">
       <HelloWorld />
     </article>
   )
