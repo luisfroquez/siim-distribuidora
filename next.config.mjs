77f2f71a104898509f7d0c06cf550c0d46002403
Luis Roque <luisfroquez@gmail.com>
2023-10-04 17:54:11 -0300

    fix: nextconfig headers

--- next.config.mjs
+++ next.config.mjs
@@ -49,6 +49,19 @@ const nextConfig = {
       },
     ]
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'all',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default withMDX(nextConfig)
